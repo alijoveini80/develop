2456be44f63d32c52c7f01108f23df8befc2101c
alijoveini80 <alijoveini80@gmail.com>
2023-08-03 17:27:58 +0330

    thrid commit

--- keyboard/keyboard.php
+++ keyboard/keyboard.php
@@ -12,5 +12,5 @@ require 'user/ostad_shenasi_buttons.php';
 require 'user/umzfood_button.php';
 require 'user/umziha_button.php';
 require 'user/umzmedia_button.php';
-require 'user/send_study_groups.php';
+require 'user/send_study_groups_button.php';
 require 'user/new_incoming_students_button.php';
