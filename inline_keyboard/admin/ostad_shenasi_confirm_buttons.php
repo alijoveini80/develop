be9d33027950941e41d8b0273db6f14f3b52d7cd
alijoveini80 <alijoveini80@gmail.com>
2023-08-04 17:25:27 +0330

    add inline keyboard

--- inline_keyboard/admin/ostad_shenasi_confirm_buttons.php
+++ inline_keyboard/admin/ostad_shenasi_confirm_buttons.php
@@ -18,6 +18,27 @@ function ostadShenasiConfirmButton($from_id, $msg_id, $reply_to)
     ]);
 }
 
+function ostadShenasiQuestionConfirmButton($from_id, $msg_id, $reply_to){
+    $deeplink = base64url_encode($from_id . "_" . $msg_id . "_report");
+    return json_encode([
+        'inline_keyboard' => [
+            [
+                ['text' => "پاسخ ارسال شده", 'callback_data' => $from_id . "_" . $msg_id . "_read-answer_" . $msg_id],
+                ['text' => "ارسال پاسخ", 'url' => "https://t.me/" . BOT_ID . "?start=" . $deeplink],
+            ],
+            [
+                /*['text' => "به اشتراک بگذارید", 'switch_inline_query' => "ostad-shenasi->" . $msg_id],*/
+                /*['text' => "کامنت ها", 'login_url' => ['url' => "https://comments.bot/thread/" . $page, 'bot_username' => "@CommentsBot"]],*/
+                ['text' => "گزارش خطا", 'url' => "https://t.me/" . BOT_ID . "?start=" . $deeplink],
+            ],
+            [
+                confirmSingleButton(false, "ostadOK", $reply_to),
+                // ['text' => "تایید", 'callback_data' => "ostadOK" . $reply_to],
+            ],
+        ],
+    ]);
+}
+
 function ostadShenasiConfirmFeedbackButton(array $rows, $reply_to = "")
 {
     $buttons = [];
