299317faaade2d94ec93ac6d26c5ee2fc4855865
alijoveini80 <alijoveini80@gmail.com>
2023-08-05 00:18:35 +0330

    add inline keyboard

--- inline_keyboard/admin/umzfood_confirm_feedback.php
+++ inline_keyboard/admin/umzfood_confirm_feedback.php
@@ -14,26 +14,3 @@ function umzfoodButton(bool $is_confirmed, $pid, $reply_to, $ref, $url = "")
         ],
     ]);
 }
-
-
-// function umzfoodConfirmFeedback(array $rows, $reply_to = "")
-// {
-//     $buttons = [];
-//     foreach ($rows as $row) {
-//         array_push($buttons, $row);
-//     }
-//     if (!empty($reply_to)) {
-//         array_push($buttons, [
-//             ['text' => "تایید (ارسال شد!)", 'callback_data' => "self-ok" . $reply_to],
-//         ]);
-//         return json_encode([
-//             'inline_keyboard' => $buttons
-//         ]);
-//     }
-//     array_push($buttons, [
-//         ['text' => "تایید (ارسال شد!)", 'callback_data' => "self-ok"],
-//     ]);
-//     return json_encode([
-//         'inline_keyboard' => $buttons
-//     ]);
-// }
