f711eebf30553f5d66d46398fd3684cf58e37474
alijoveini80 <alijoveini80@gmail.com>
2023-08-04 17:44:41 +0330

    add inline keyboard

---
+++ inline_keyboard/admin/new_incomoming_students_confirm_button.php
@@ -0,0 +1,16 @@
+<?php
+
+function groupsConfirmButton(bool $is_confirmed, $pid, $reply_to, $ref, $url = "")
+{
+    return json_encode([
+        'inline_keyboard' => [
+            [
+                sendMessageSingleButton($url),
+            ],
+            [
+                adminViewIdSingleButton($pid),
+                confirmSingleButton($is_confirmed, "vizhe-ok-new", $reply_to, $ref),
+            ],
+        ],
+    ]);
+}
